72d0175e55da4e795f664298feb8fe7a384aa047
shivangi <shivangisoti18@gmail.com>
2025-10-23 12:41:04 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          own project <code>src/App.jsx</code> Shivangi soti
         </p>
       </div>
       <p className="read-the-docs">
